b6818c3d61025cca9bb4a0ec044d67ba49478ad3
malvarez27 <malvar52@lion.lmu.edu>
2017-09-16 19:38:17 -0700

    interleave and crypto

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,4 +1,5 @@
 // This is HW 1
+const crypto = require('crypto');
 
 function change (amount) {
 	if (amount < 0){
@@ -14,6 +15,11 @@ function change (amount) {
 	return result;
 };
 
+function stripQuotes(str) {
+  let final = str.replace(/'/g, '');
+  final = final.replace(/"/g, '');
+  return final;
+}
 
 function scramble (word) {
 	if (word.length === 0) {
@@ -32,14 +38,53 @@ function powers(base, limit, callback) {
 }
 
 function* powersGenerator(base, limit) {
-	//const newMath = 0;
 	for (let i = 0; base ** i <= limit; i += 1) {
     const input = base ** i;
     yield input;
 }
 }
 
+function interleave(...args){
+  let r = args.shift();
+  let newr = args;
+  let rest = [];
+  if (r.length === 0){
+  	return newr;
+  } else if (newr.length ===0) {
+  	return r;
+  } else {
+	  while (r.length !== 0 && newr.length !==0) {
+	    rest.push(r.shift());
+	    rest.push(newr.shift());
+	    if (r.length === 0) {
+	    	return rest.concat(newr);
+	    } else if (newr.length === 0) {
+			return rest.concat(r);
+	    }
+	 }
+  }
+}
+ function makeCryptoFunctions(password, algorithm){
+ 	const localPass = password;
+ 	const localAlg = algorithm;
+	return [
+		function encrypt(text){
+
+		  let cipher = crypto.createCipher(localAlg,localPass);
+		  let crypt = cipher.update(text,'utf8','hex');
+		  crypt += cipher.final('hex');
+		  return crypt;
+		},
+	 
+		function decrypt(text){
+		  let decipher = crypto.createDecipher(localAlg,localPass);
+		  let decry = decipher.update(text,'hex','utf8');
+		  decry += decipher.final('utf8');
+		  return decry;
+		}
+	];
+}
 	
 module.exports = {
-	change, scramble, powers, powersGenerator,
+	change, stripQuotes, scramble, powers, powersGenerator, interleave,makeCryptoFunctions,
 };
