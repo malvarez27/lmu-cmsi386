0f65b788cd4fd0645c8414c547f914166682185e
malvarez27 <malvar52@lion.lmu.edu>
2017-10-04 15:17:35 -0700

    not done

---
+++ homework2/warmup.py
@@ -0,0 +1,75 @@
+import random
+from Crypto.Cipher import AES
+
+
+def make_crypto_functions(key, initialization_vector):
+	local_key = key
+	local_initialization_vector = initialization_vector
+	def encrypt(text):
+		cipher = AES.new(local_key, AES.MODE_CBC, local_initialization_vector)
+		return cipher.encrypt(text)
+
+	def decrypt(text):
+		cipher = AES.new(local_key, AES.MODE_CBC, local_initialization_vector)
+		return cipher.decrypt(text)
+		
+
+	return (encrypt, decrypt)
+
+"""  5 triples."""
+def triples():
+   for c in range(1, 100):
+     for b in range(1, c):
+       for a in range(1, b):
+         if a * a + b * b == c * c:
+           print('({},{},{})'.format(a, b, c))
+
+
+def scramble(words):
+	permuted_words = random.sample(words, len(words))
+	new_words = ''.join(permuted_words)
+	return new_words
+
+# print(scramble(""))
+
+def powers(power):
+	num = 0
+	while num < power:
+		yield num
+		num += 1
+
+
+def interleave(first, *second):
+	one_length = len(first)
+	two_length = len(second)
+	max_num = math.max(len(first), len(second))
+	result = []
+	for x in range(0, max_num):
+		if (x < len(first)):
+			result.append(first[x])
+		if (x < len(second)):
+			result.append(second[x])
+	return result
+
+def change():
+	print("asdfasd")
+
+def strip_quotes():
+	print("hi")
+
+def say():
+	print ("hi")
+
+def Cylinder():
+	print("hi")
+
+def random_name():
+	print("hi")
+
+
+
+
+# if __name__ == "__main__":
+# 	e, d = make_crypto_functions('zombie devops feynman123', '0000000000000000')
+# 	print(e)
+# 	print(d)
