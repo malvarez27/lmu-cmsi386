e8888c37ccec2c1523242b3a3388c617823fb6ef
malvarez27 <malvar52@lion.lmu.edu>
2017-11-08 15:06:27 -0800

    added some code for queue

---
+++ homework3/queue.h
@@ -0,0 +1,16 @@
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Queue {
+
+  struct Node{
+    T data;
+    Node* next;
+  };
+  int size = 0;
+  
+  }
+}
