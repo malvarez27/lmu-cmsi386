7e0cec5bd15263418142a16dc4e2b3f0fc6a9890
malvarez27 <malvar52@lion.lmu.edu>
2017-11-09 15:36:00 -0800

    queue

--- homework3/queue.h
+++ homework3/queue.h
@@ -6,11 +6,46 @@ using namespace std;
 template <typename T>
 class Queue {
 
-  struct Node{
+  struct Node {
     T data;
     Node* next;
   };
+
   int size = 0;
-  
-  }
-}
+  Node* tail = nullptr;
+  Node* head = nullptr;
+
+public:
+  ~Queue() {
+      while (tail != nullptr) {
+        //dequeue();
+      }
+    }
+    Queue() = default;
+    Queue(const Queue& s)= delete;
+    Queue& operator = (const Queue& s) = delete;
+
+    int get_size(){
+      return size;
+    }
+    T get_tail() {
+    return tail->data;
+    }
+
+    void enqueue(T x) {
+      tail = new Node {x, tail};
+      if (head == nullptr){
+        head = tail;
+      }
+      size++;
+     }
+
+     T dequeue() {
+     Node* nodeToDelete = head;
+     T valueToReturn = head->data;
+     head = head->next;
+     size--;
+     delete nodeToDelete;
+     return valueToReturn;
+     }
+   };
